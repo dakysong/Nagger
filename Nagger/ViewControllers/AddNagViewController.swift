3be6b0ce691e7cf1160c59cadcb1a4e361cbc5ac
dak-song <daksong@sas.upenn.edu>
2018-10-04 20:32:58 -0400

    First commit

---
+++ Nagger/ViewControllers/AddNagViewController.swift
@@ -0,0 +1,49 @@
+//
+//  AddNagViewController.swift
+//  Nagger
+//
+//  Created by Dak Song on 10/3/18.
+//  Copyright © 2018 Dak Song. All rights reserved.
+//
+
+import UIKit
+
+class AddNagViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier, identifier == "VCTwoSegueIdentifier" {
+            let vcTwo = segue.destination
+            vcTwo.view.backgroundColor = UIColor.green
+        }
+    }
+
+    @IBAction func vcTwoTapped(_ sender: UIButton) {
+//        performSegue(withIdentifier: "VCOneSegueIdentifier", sender: self)
+        if let vcTwo = storyboard?.instantiateViewController(withIdentifier: "VCTwoIdentifier") {
+            show(vcTwo, sender: self)
+        }
+    }
+    
+}
---
+++ Nagger/ViewControllers/ViewControllerTwo.swift
@@ -0,0 +1,34 @@
+//
+//  ViewControllerTwo.swift
+//  Nagger
+//
+//  Created by Dak Song on 10/3/18.
+//  Copyright © 2018 Dak Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTwo: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.blue
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
